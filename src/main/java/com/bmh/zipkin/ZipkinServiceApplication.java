5ee4b0ff3d80bad2c384079980df1e099281184a
Mohamed Babchia <mbabchia@gmail.com>
2017-04-10 17:18:05 +0200

    initial commit

---
+++ src/main/java/com/bmh/zipkin/ZipkinServiceApplication.java
@@ -0,0 +1,19 @@
+package com.bmh.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin.server.EnableZipkinServer;
+
+
+
+@EnableDiscoveryClient
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServiceApplication.class, args);
+	}
+}
